00ae885769b21165eae3cb35ea00147d371eea46
Kleber Amaro <kleberamarosilva@gmail.com>
2026-09-17 12:06:07 -0300

    primeira aula de react

---
+++ aula7/projeto14/projeto/frontEndReact/src/Menu.jsx
@@ -0,0 +1,17 @@
+import "./Menu.css"
+
+function Menu(){
+    return(
+    <div className="menu">
+      <ul>
+        <li>Home</li>
+        <li>Quem somos</li>
+        <li>Contato</li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+    )
+}
+
+export default Menu;
